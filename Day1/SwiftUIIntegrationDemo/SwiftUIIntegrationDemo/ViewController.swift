f28e9c52affc6ab6885a5cedf6922192625ea1e2
hazem habeb <hazemhabeb94@gmail.com>
2025-10-22 18:58:52 +0300

    Swift UI Integration

---
+++ Day1/SwiftUIIntegrationDemo/SwiftUIIntegrationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIIntegrationDemo
+//
+//  Created by hazemhabeb on 22/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day1/SwiftUIIntegrationDemo/SwiftUIIntegrationDemoUITests/SwiftUIIntegrationDemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUIIntegrationDemoUITestsLaunchTests.swift
+//  SwiftUIIntegrationDemoUITests
+//
+//  Created by hazemhabeb on 22/10/2025.
+//
+
+import XCTest
+
+final class SwiftUIIntegrationDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
