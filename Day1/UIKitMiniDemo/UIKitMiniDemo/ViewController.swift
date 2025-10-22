906bd38023430025e0a7a2cd6a70afb8825533de
hazem habeb <hazemhabeb94@gmail.com>
2025-10-22 18:26:16 +0300

    UIKit Practice

---
+++ Day1/UIKitMiniDemo/UIKitMiniDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitMiniDemo
+//
+//  Created by hazemhabeb on 22/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
