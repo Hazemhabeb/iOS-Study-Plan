5cf2eefe48dbe9c19901355cc5fc4ef0eb93d853
hazem habeb <hazemhabeb94@gmail.com>
2025-10-24 22:54:08 +0300

    mvvm with async

---
+++ Day1/QuotesViewe_MVVVM/QuotesViewe_MVVVM/QuotesViewe_MVVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesViewe_MVVVMApp.swift
+//  QuotesViewe_MVVVM
+//
+//  Created by hazemhabeb on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuotesViewe_MVVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
