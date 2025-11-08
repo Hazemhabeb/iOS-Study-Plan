fbad06cb8b99aee1f4cc5f1b96bd20bdcb046a1c
hazem habeb <hazemhabeb94@gmail.com>
2025-11-08 10:41:07 +0200

    added actor fix

--- DataRace.playground/Contents.swift
+++ DataRace.playground/Contents.swift
@@ -25,7 +25,7 @@ DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
 }
 
 //this is example of what is the data race is
-//MARK: - Fixing with Serial Queue
+//MARK: -1 Fixing with Serial Queue
 
 class SafeCounter {
     private var value = 0
@@ -91,7 +91,7 @@ DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
     print("🟣 barrier Counter Value : \( barrierCounter.getValue())") //Always 1000
 }
 
-//MARK: Fixing with NSLock / NSRecursiveLock
+//MARK: -2 Fixing with NSLock / NSRecursiveLock
 
 class LockedCounter {
     private var value = 0
@@ -126,3 +126,68 @@ DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
 ///NSRecursiveLock
 ///Used if a thread might lock the same lock multiple times recursively.
 
+//MARK: -3 Swift Concurrency way : @Sendable and Actors
+
+func perform (_ closure : @escaping @Sendable () -> Void) {
+    DispatchQueue.global().async(execute: closure)
+}
+///If your closure captures non-thread-safe variables, compiler warns you.
+///Helps prevent hidden data races.
+///
+
+//MARK: 6-Actors Modern Thread Save Model
+
+///swift actors automatically provide mutual exclusion for their internal state
+///"Classes that guarantee thread safety — only one task can access their properties at a time."
+///
+//Example — Broken Version (Without Actor)
+
+class CounterWithoutActor {
+    var value = 0
+}
+
+let counter2 = CounterWithoutActor()
+
+Task.detached {
+    for _ in 0..<1000 { counter2.value += 1 }
+}
+
+Task.detached {
+    for _ in 0..<1000 { counter2.value += 1 }
+}
+
+Task {
+    try await Task.sleep(nanoseconds: 500_000_000)
+    print("🔴 Corrupted Value: \(counter2.value)")
+}
+
+//MARK: Fixed Version with Actor
+
+actor CounterActor {
+    var value = 0
+    
+    func increment() {
+        value += 1
+    }
+    
+    func getValue() -> Int{
+        return value
+    }
+}
+
+let safeActor = CounterActor()
+
+Task.detached {
+    for _ in 0..<1000 { await safeActor.increment() }
+}
+
+Task.detached {
+    for _ in 0..<1000 { await safeActor.increment() }
+}
+
+Task {
+    try await Task.sleep(nanoseconds: 500_000_000)
+    print("🔵 Actor Safe Value: \(await safeActor.getValue())") //Always 2000
+}
+///💡 The actor ensures all access is serialized automatically.
+///You never need locks, barriers, or queues.
